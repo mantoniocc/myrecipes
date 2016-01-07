fff607f35038d2dcaa1cb048171dadb5b0b241ac
Mario Cortes <mcortes.it@outlook.com>
2016-01-07 05:38:10 -0300

    create chefs resources and authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,15 @@
 class RecipesController < ApplicationController
 
+	before_action :set_recipe, only: [:edit, :update, :show, :like]
+	before_action :require_user, except: [:show, :index]
+	before_action :require_same_user, only: [:edit, :update]
+
 	def index
 		@recipes = Recipe.paginate(page: params[:page], per_page: 4)
 	end
 
 	def show
-		@recipe = Recipe.find(params[:id])
+		
 	end
 
 	def new
@@ -14,7 +18,7 @@ class RecipesController < ApplicationController
 
 	def create
 		@recipe = Recipe.new(recipe_params)
-		@recipe.chef = Chef.find(2)
+		@recipe.chef = current_user
 
 		if @recipe.save
 			flash[:success] = "Your recipe was created succesfully!"
@@ -25,11 +29,10 @@ class RecipesController < ApplicationController
 	end
 
 	def edit
-		@recipe = Recipe.find(params[:id])
+		
 	end
 
 	def update
-		@recipe = Recipe.find(params[:id])
 		if @recipe.update(recipe_params)
 			flash[:success] = "Your recipe was updated succesfully!"
 			redirect_to @recipe
@@ -39,8 +42,7 @@ class RecipesController < ApplicationController
 	end
 
 	def like
-		@recipe = Recipe.find(params[:id])
-		like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+		like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
 		if like.valid?
 			flash[:success] = "Your selection was succesful"
 			redirect_to :back
@@ -53,5 +55,16 @@ class RecipesController < ApplicationController
 	private
 		def recipe_params
 			params.require(:recipe).permit(:name, :summary, :description, :picture)
+		end
+
+		def set_recipe
+			@recipe = Recipe.find(params[:id])
+		end
+
+		def require_same_user
+			if current_user != @recipe.chef
+				flash[:danger] = "You can edit your own recipes"
+				redirect_to recipes_path
+			end
 		end
 end
